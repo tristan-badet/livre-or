a9727e48ea7bd593181e1fd69f3b8dc035c572f0
tristan-badet <127488323+tristan-badet@users.noreply.github.com>
2023-06-14 16:24:50 +0200

    Class Goldenbook faite

--- livre-or.php
+++ livre-or.php
@@ -7,13 +7,33 @@ header("location:connexion.php");
 exit;
 }
 require 'database.php';
-$error = "";
-$query = $bdd->prepare('SELECT * FROM comment ORDER BY date DESC');
-$query->execute();
-$comments = $query->fetchall();
-$query2 = $bdd->prepare('SELECT login FROM user JOIN comment ON user.id = comment.id_user ORDER BY comment.date DESC');
-$query2->execute();
-$username = $query2->fetchAll(PDO::FETCH_COLUMN);
+
+class GoldenBook{
+    private $database;
+
+    public function __construct($database){
+        $this->database = $database;
+    }
+
+    public function getComments() {
+        $bdd = $this->database->getBdd();
+        $error = "";
+        $query = $bdd->prepare('SELECT * FROM comment ORDER BY date DESC');
+        $query->execute();
+        $comments = $query->fetchall();
+        $query2 = $bdd->prepare('SELECT login FROM user JOIN comment ON user.id = comment.id_user ORDER BY comment.date DESC');
+        $query2->execute();
+        $usernames = $query2->fetchAll(PDO::FETCH_COLUMN);
+        return array('comments' => $comments, 'usernames' => $usernames);
+    }
+}
+
+$database = new Database();
+$goldenBook = new GoldenBook($database);
+$data = $goldenBook->getComments();
+
+$comments = $data['comments'];
+$usernames = $data['usernames'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
